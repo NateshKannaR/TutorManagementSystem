02b1ab099fb3aac79a568d3e809b96a0c1ccc256
NateshKannaR <nateshnkraja@gmail.com>
2025-09-03 16:41:49 +0000

    Wed Sep 03 2025 16:41:48

--- springapp/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (username, password, email, role) VALUES 
-('Natesh', 'password', 'natesh@example.com', 'TUTOR'),
-('admin', 'password', 'admin@example.com', 'SYSTEM_ADMINISTRATOR');
